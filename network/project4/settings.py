56b0083c5d840527f6f5cf299b36aed20b7abe22
anees-72 <mohsinmohsinali775@gmail.com>
2025-03-22 07:50:10 +0000

    modified settings.py

--- network/project4/settings.py
+++ network/project4/settings.py
@@ -129,4 +129,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS=[BASE_DIR / 'network' /'static']
+STATICFILES_DIRS=[BASE_DIR / 'network' /'static' /'network']
