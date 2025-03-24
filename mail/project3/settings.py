b5a8c8a34fbe9e99af7a549569cec7a6e86a66b4
anees-72 <mohsinmohsinali775@gmail.com>
2025-03-24 05:11:29 +0000

    added whitenoise for better static serving

--- mail/project3/settings.py
+++ mail/project3/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -53,6 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ROOT_URLCONF = 'project3.urls'
 
 TEMPLATES = [
